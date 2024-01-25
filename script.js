bc0032423c942761f60690c84a9f88a3cf0e259e
Nate <106435163+Natebrook11@users.noreply.github.com>
2024-01-25 18:49:37 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -26,3 +26,42 @@ window.onscroll = () => {
   });
 };
 });
+
+const tl = gsap.timeline();
+
+tl.to("body", {
+  overflow: "hidden"
+})
+  .to(".preloader .text-container", {
+    duration: 0,
+    opacity: 1,
+    ease: "Power3.easeOut"
+  })
+  .from(".preloader .text-container h5", {
+    duration: 1.5,
+    delay: 1,
+    y: 70,
+    stagger: 0.4,
+    ease: "Power3.easeOut"
+  })
+  .to(".preloader .text-container h5", {
+    duration: 1.2,
+    y: 70,
+    stagger: 0.2,
+    ease: "Power3.easeOut"
+  })
+  .to(".preloader", {
+    duration: 1.5,
+    height: "0vh",
+    ease: "Power3.easeOut"
+  })
+  .to(
+    "body",
+    {
+      overflow: "auto"
+    },
+    "-=2"
+  )
+  .to(".preloader", {
+    display: "none"
+  });
